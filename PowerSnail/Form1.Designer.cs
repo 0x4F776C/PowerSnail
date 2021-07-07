08cb95de8e87ba9d9b6201a5c98858f3bd780169
0x4F776C <41299140+0x4F776C@users.noreply.github.com>
2021-07-07 13:13:07 +0800

    Add files via upload

---
+++ PowerSnail/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace PowerSnail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmdSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdInput = new System.Windows.Forms.TextBox();
+            this.cmdOutput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdSubmit
+            // 
+            this.cmdSubmit.Location = new System.Drawing.Point(519, 10);
+            this.cmdSubmit.Name = "cmdSubmit";
+            this.cmdSubmit.Size = new System.Drawing.Size(75, 23);
+            this.cmdSubmit.TabIndex = 0;
+            this.cmdSubmit.Text = "&Run";
+            this.cmdSubmit.UseVisualStyleBackColor = true;
+            this.cmdSubmit.Click += new System.EventHandler(this.cmdSubmit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Command:";
+            // 
+            // cmdInput
+            // 
+            this.cmdInput.Location = new System.Drawing.Point(66, 12);
+            this.cmdInput.Name = "cmdInput";
+            this.cmdInput.Size = new System.Drawing.Size(447, 20);
+            this.cmdInput.TabIndex = 2;
+            // 
+            // cmdOutput
+            // 
+            this.cmdOutput.BackColor = System.Drawing.Color.Black;
+            this.cmdOutput.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdOutput.ForeColor = System.Drawing.Color.White;
+            this.cmdOutput.Location = new System.Drawing.Point(6, 38);
+            this.cmdOutput.Multiline = true;
+            this.cmdOutput.Name = "cmdOutput";
+            this.cmdOutput.ReadOnly = true;
+            this.cmdOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.cmdOutput.Size = new System.Drawing.Size(588, 400);
+            this.cmdOutput.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(407, 441);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "GitHub: https://github.com/0x4F776C";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.cmdSubmit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 458);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmdOutput);
+            this.Controls.Add(this.cmdInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdSubmit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PowerSnail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdSubmit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox cmdInput;
+        private System.Windows.Forms.TextBox cmdOutput;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PowerSnail/Form1.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * PowerShell Alternative - Executable
+ * Created:
+ *  By:         Lee Chun Hao
+ *  On:         11th August 2020
+ *  Purpose:    Run PowerShell commands/scripts even when PowerShell was blacklisted
+ * Link:
+ *  Linkedin:   https://sg.linkedin.com/in/lee-chun-hao-602ba670
+ *  GitHub:     https://github.com/0x4F776C
+ *  HackTheBox: https://www.hackthebox.eu/profile/152036
+ */
+
+using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+
+namespace PowerSnail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string RunCommand(string script)
+        {
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+
+            Pipeline pipeline = runspace.CreatePipeline();
+            pipeline.Commands.AddScript(script);
+            pipeline.Commands.Add("Out-String");
+
+            Collection<PSObject> results = pipeline.Invoke();
+
+            runspace.Close();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (PSObject psObject in results)
+                stringBuilder.AppendLine(psObject.ToString());
+            return stringBuilder.ToString();
+        }
+
+        private void cmdSubmit_Click(object sender, EventArgs e)
+        {
+            String policyBypass = "Set-ExecutionPolicy Unrestricted -Scope CurrentUser;";
+
+            cmdOutput.Clear();
+            cmdOutput.Text = RunCommand(policyBypass + cmdInput.Text);
+        }
+    }
+}
